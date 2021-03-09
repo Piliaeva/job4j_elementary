c8f741b0b1bbba36f1f78c4ca1f86e9051f0292a
Piliaeva <piliaeva@bpcbt.com>
2021-03-09 08:21:17 +0300

    5.0.1. Сумма чисел. [#442452]

--- src/ru/job4j/ru/job4j/loop/Counter.java
+++ src/ru/job4j/ru/job4j/loop/Counter.java
@@ -1,7 +1,6 @@
 package ru.job4j.ru.job4j.loop;
 
 public class Counter {
-    @SuppressWarnings("checkstyle:ParenPad")
     public static int sum(int start, int finish) {
         int sum = 0;
         for (int a = start; a <= finish; a++) {
