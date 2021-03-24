ad467d6b9921b4ad3d9ce5d3c7c720bf15e24b0c
Piliaeva <piliaeva@bpcbt.com>
2021-03-24 22:24:25 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#442455]

--- src/ru/job4j/ru/job4j/loop/Counter.java
+++ src/ru/job4j/ru/job4j/loop/Counter.java
@@ -9,11 +9,28 @@ public class Counter {
         return sum;
     }
 
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int a = start; a <= finish; a++) {
+            if (a % 2 == 0) {
+                sum = sum + a;
+            }
+
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
         System.out.println(sum(0, 20));
         System.out.println(sum(5, 6));
+
+        System.out.println("\n");
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
---
+++ test/ru/job4j/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CounterTest {
+
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        Assert.assertEquals(expected, result);
+    }
+    @Test
+    public void whenSumEvenNumbersFromOneToOneThenZero() {
+        int start = 1;
+        int finish = 1;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+    @Test
+    public void whenSumEvenNumbersFromZeroToOneThenZero() {
+        int start = 0;
+        int finish = 1;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+    @Test
+    public void whenSumEvenNumbersFromZeroToFiveThenSix() {
+        int start = 0;
+        int finish = 5;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 6;
+        Assert.assertEquals(expected, result);
+    }
+}
