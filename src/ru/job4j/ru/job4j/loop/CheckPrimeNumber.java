004907db464c9c5464e2161efa81deb72342306b
Piliaeva <piliaeva@bpcbt.com>
2021-04-14 08:30:30 +0300

    5.5. Простое число [#442450]

--- src/ru/job4j/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/ru/job4j/loop/CheckPrimeNumber.java
@@ -10,7 +10,6 @@ public class CheckPrimeNumber {
                 prime = false;
                 break;
             } else if (index == next) {
-                prime = true;
                 break;
             }
         }
