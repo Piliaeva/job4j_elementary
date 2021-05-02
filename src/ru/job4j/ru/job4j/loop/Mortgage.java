4f19777ba68d7178c78a3c8800fc0c2acf232366
Piliaeva <piliaeva@bpcbt.com>
2021-05-02 12:36:18 +0300

    5.7. Ипотека [#442449]

--- src/ru/job4j/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,10 @@ package ru.job4j.ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        double ostatok = amount + (amount * (percent / 100));
-        while (ostatok > 0) {
-            ostatok = ostatok - salary;
-            year += 1; }
+        while (amount > 0) {
+            amount += amount * percent / 100 - salary;
+            year++;
+        }
         return year;
     }
 }
