6395da3941c0d770d4ccdee6eb160436187fbeb6
Piliaeva <piliaeva@bpcbt.com>
2021-05-02 20:35:38 +0300

    6.0.2. Заполнение массива. [#442371]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,13 +2,15 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        System.out.println(ages.length);
 
-        String[] surnames = new String[100500];
-        System.out.println(surnames.length);
-
-        float[] prices = new float[40];
-        System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Vasiliy Ivanov";
+        names[1] = "Daria Ivanova";
+        names[2] = "Ivan Ivanov";
+        names[3] = "Olga Vasilieva";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
